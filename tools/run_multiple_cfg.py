981dd0ab796620f50ee519315e7461f126991c23
pjh3974 <pjh3974@gmail.com>
2021-12-17 09:06:27 +0000

    change cfg

--- tools/run_multiple_cfg.py
+++ tools/run_multiple_cfg.py
@@ -3,7 +3,8 @@ import json
 import os
 
 parser = argparse.ArgumentParser("Multiple cfg runner")
-parser.add_argument("--cfg-json", type=str, default="experiments/EoZP_ballons.json")
+parser.add_argument("--cfg-json", type=str, default="experiments/EoZP.json")
+parser.add_argument("--image", type=str, default="ballons")
 parser.add_argument("--log-file", type=str, default="log.txt")
 
 args = parser.parse_args()
@@ -15,20 +16,20 @@ template_script = "python -m torch.distributed.launch --nproc_per_node  2 --mast
 					--cfg-options {cfg_opt} --launcher pytorch"
 
 WORK_ROOT = "output"
-unique_name = args.cfg_json.split('.')[0]
+unique_name = str.join('_', [args.cfg_json.split('.')[0], args.image])
 error_list = []
 cfg_name = os.path.basename(args.cfg_json).split('.')[0]
 
 for exp_dict in exp_list:
-	exp_name = exp_dict["exp_name"]
-	work_dir = os.path.join(WORK_ROOT, cfg_name, exp_name)
+	exp_name = exp_dict["exp_name"].format(image=args.image)
+	work_dir = os.path.join(WORK_ROOT, str.join('_',[cfg_name, args.image]), exp_name)
 
 	exp_end = os.path.isdir(os.path.join(work_dir, "final"))
 
 	if exp_end:
 		continue
 
-	cfg_file = exp_dict["cfg_file"]
+	cfg_file = exp_dict["cfg_file"].format(image=args.image)
 	cfg_opt = str.join(' ',["{}={}".format(k, v) for k, v in exp_dict["cfg_opt"].items()])
 	curr_script = template_script.format(cfg_file=cfg_file, work_dir=work_dir, cfg_opt=cfg_opt)
 
@@ -36,7 +37,7 @@ for exp_dict in exp_list:
 		os.system(curr_script)
 	except Exception as e:
 		print(str(e))
-		exp_name = exp_dict["exp_name"]
+		exp_name = exp_dict["exp_name"].format(image=args.image)
 		error_list.append(exp_name)
 
 log_file_path = os.path.join(WORK_ROOT, unique_name, args.log_file)
